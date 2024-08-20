2748e6c5578a94a0dbd507a83909591bf44d0854
Chris Apple <cja-private@pm.me>
2024-08-20 08:56:44 -0700

    Very important hi world update (#1)

--- startup.sh
+++ startup.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo "hi world"
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' > $HOME/.zprofile
